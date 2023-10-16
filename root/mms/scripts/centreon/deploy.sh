2ea164022bec70af99bfeb9d1b375da12579b30b
root <root@localhost.localdomain>
2023-10-17 01:43:15 +0200

    Update de centreon-plugins

--- root/mms/scripts/centreon/deploy.sh
+++ root/mms/scripts/centreon/deploy.sh
@@ -2,8 +2,10 @@
 
 echo "ENV CENTREON_PLUGINS_VERSION = ${CENTREON_PLUGINS_VERSION}"
 
-CENTREON_PLUGINS_VERSION="20230118"
+CENTREON_PLUGINS_VERSION="20231017"
 
-curl -LO https://github.com/centreon/centreon-plugins/archive/refs/tags/${CENTREON_PLUGINS_VERSION}.tar.gz
+# curl -LO https://github.com/centreon/centreon-plugins/archive/refs/tags/${CENTREON_PLUGINS_VERSION}.tar.gz
+curl -LO https://github.com/handfreezer/z_centreon-plugins/archive/refs/tags/${CENTREON_PLUGINS_VERSION}.tar.gz
+
 tar xzvf ${CENTREON_PLUGINS_VERSION}.tar.gz
-ln -sf centreon-plugins-${CENTREON_PLUGINS_VERSION}/src src
+ln -sf *centreon-plugins-${CENTREON_PLUGINS_VERSION}/src src
